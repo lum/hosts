cf5ab13d7217f127c931912f3311a0b23c3bed6e
Steve Lum <steve.lum@gmail.com>
2013-05-30 14:34:54 -0700

    updating default recipe to specify hostname using value in /etc/hostname

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,23 @@
-hosts = search(:node, "*:*")
+#
+# Cookbook Name:: hosts
+# Recipe:: client
+#
+# Copyright 2013, Tempo
+#
+#
 
 template "/etc/hosts" do
   source "hosts.erb"
   owner "root"
   group "root"
   mode 0644
-  variables(:hosts => hosts)
-  not_if "grep 'falafel shop' /etc/hosts"
+end
+
+execute "hostname --file /etc/hostname" do
+  action :nothing
+end
+
+file "/etc/hostname" do
+  content "#{node[:fqdn]}"
+  notifies :run, resources(:execute => "hostname --file /etc/hostname"), :immediately
 end
