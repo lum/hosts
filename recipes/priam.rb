fcbde84281a3c56fe7913161aedb0c7d8b3b86ad
Steve <steve.lum@gmail.com>
2014-07-22 17:26:03 -0700

    added new priam recipe to use with priam cassandra nodes. Version bump of cookbook to 0.3.0

---
+++ recipes/priam.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: hosts
+# Recipe:: default
+# Author:: Steve Lum <steve.lum@gmail.com>
+#
+# Copyright 2013, Tempo
+#
+
+if !Chef::Config[:solo]
+	hosts = search(:node, "chef_environment:#{node.chef_environment}")
+else
+  hosts = node['ipaddress']
+end
+
+template "/etc/hosts" do
+  source "hosts.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  variables(:hosts => hosts)
+end
+
+new_hostname = "#{node.name}"
+new_fqdn = "#{new_hostname}.#{node['cluster']}.tempo.ai"
+
+execute "hostname --file /etc/hostname" do
+  action :nothing
+end
+
+file "/etc/hostname" do
+  content "#{node.name}.#{node['cluster']}.tempo.ai"
+  notifies :run, resources(:execute => "hostname --file /etc/hostname"), :immediately
+  ignore_failure true
+end
+
+# update the node attributes during the chef run, so that the new values can be used later
+node.automatic_attrs["hostname"] = new_hostname
+node.automatic_attrs["fqdn"] = new_fqdn
